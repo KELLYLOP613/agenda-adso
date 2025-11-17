97b5c6cf895f4135f4a37241000e58c3fedf3944
Kelly Lopera <johalopera15@gmail.com>
2025-11-17 12:00:22 -0500

    Clase_6_Agenda_ADSO_v4_Tailwind

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react';  /*React: libreria principal*/
-import ReactDOM from 'react-dom/client';  /*ReactDOM: dibuja React en el navegador*/
-import App from './App.jsx'; /*Importamos el compopnente raiz(nuestra agenda ADSO)*/
-import './index.css'; /*Importamos los estilos globales*/ 
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'   // <--- IMPORTANTE
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  <App />
-  </React.StrictMode>
-);
+    <App />
+  </React.StrictMode>,
+)
